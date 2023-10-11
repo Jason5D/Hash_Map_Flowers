b1ff81c27f68021817ae5722a46d64c871875f55
Jason <jason5donoghue@gmail.com>
2023-10-11 15:06:24 +0100

    hash and compress methods added

--- script.py
+++ script.py
@@ -3,3 +3,13 @@ class HashMap:
     def __init__(self, size):
         self.array_size = size
         self.array = [None for i in range(size)]
+
+    # Hash method
+    def hash(self, key):
+        key_bytes = key.encode()
+        hash_code = sum(key_bytes)
+        return hash_code
+
+    # Compression method
+    def compressor(self, hash_code):
+        return hash_code % self.array_size
